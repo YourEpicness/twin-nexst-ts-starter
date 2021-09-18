538cd3ea2dfe16d1095a9152ee6d54ade9c7d4e2
Julaam Diop <jeebe.diop@gmail.com>
2021-09-17 19:23:30 -0500

    Add mongoDB module with ORM to NestJS

--- src/server/app.module.ts
+++ src/server/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common'
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config'
+import { MongooseModule } from '@nestjs/mongoose'
 import { ViewModule } from './modules/view/view.module'
 
 @Module({
-  imports: [ConfigModule.forRoot({isGlobal: true,}) ,ViewModule],
-  controllers: [],
-  providers: []
+	imports: [
+		ConfigModule.forRoot({ isGlobal: true }),
+		ViewModule,
+		MongooseModule.forRoot('mongodb://localhost/nest'),
+	],
+	controllers: [],
+	providers: [],
 })
 export class AppModule {}
