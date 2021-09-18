f570cc224ab85d82c30bf7c2d8e4d0c0d5196302
Julaam Diop <jeebe.diop@gmail.com>
2021-09-18 01:26:32 -0500

    Comment out mongoose ORM... might replace with Prisma or typeORM

--- src/server/app.module.ts
+++ src/server/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
-import { MongooseModule } from '@nestjs/mongoose'
+// import { MongooseModule } from '@nestjs/mongoose'
 import { ViewModule } from './modules/view/view.module'
 
 @Module({
 	imports: [
 		ConfigModule.forRoot({ isGlobal: true }),
 		ViewModule,
-		MongooseModule.forRoot('mongodb://localhost/nest'),
+		// MongooseModule.forRoot('mongodb://localhost/nest'),
 	],
 	controllers: [],
 	providers: [],
